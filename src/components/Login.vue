363bd46bf3d0295689924c567d3d7d6c161a04a7
mohammadhayaty <mahha1373@gmail.com>
2024-02-18 19:28:56 +0330

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+import { ref } from "vue";
+import { useAuthStore } from "../stores/auth";
+
+const authStore = useAuthStore();
+
+const form = ref({
+    email: "",
+    password: "",
+});
+
+</script>
+<template>
+   <div>
+ <form  @submit.prevent="authStore.handleLogin(form)">
+    @CSRF
+  <label for="fname">First name:</label><br>
+  <input type="email" id="fname" name="fname" v-model="form.email" value="John"><br>
+  <div v-if="authStore.errors.email" class="flex">
+    <span class="text-red-400 text-sm m-2 p-2">{{
+         authStore.errors.email[0]
+    }}</span>
+</div>
+  <label for="lname">Last name:</label><br>
+  <input type="password" id="lname" name="lname" v-model="form.password" value="Doe"><br><br>
+  <div v-if="authStore.errors.password" class="flex">
+   <span class="text-red-400 text-sm m-2 p-2">{{
+    authStore.errors.password[0]
+    }}</span>
+   </div>
+  <input type="submit" value="Submit">
+</form> 
+<router-link to="/forgot-password" class="text-primary hover:underline">
+ Forgot Password?
+</router-link>
+
+   </div> 
+</template>
---
+++ src/components/Register.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { ref } from 'vue';
+import { useAuthStore } from "../stores/auth";
+
+const authStore = useAuthStore();
+const form = ref({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+});
+
+</script>
+<template>
+  <section class="bg-[#F4F7FF] py-20 lg:py-[120px]">
+    <!--
+    This example requires updating your template:
+
+    ```
+    <html class="h-full bg-white">
+    <body class="h-full">
+    ```
+  -->
+  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign Up</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" @submit.prevent="authStore.handleRegister(form)">
+
+        <div>
+          <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+          <div class="mt-2">
+            <input id="name" name="name" type="text" v-model="form.name" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div v-if="authStore.errors.name" class="flex">
+                  <span class="text-red-400 text-sm m-2 p-2">{{
+                    authStore.errors.name[0]
+                  }}</span>
+                </div>
+        </div>
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+          <div class="mt-2">
+            <input id="email" name="email" type="email" v-model="form.email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div v-if="authStore.errors.email" class="flex">
+                  <span class="text-red-400 text-sm m-2 p-2">{{
+                    authStore.errors.email[0]
+                  }}</span>
+                </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+          </div>
+          <div class="mt-2">
+            <input id="password" name="password" type="password" v-model="form.password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div v-if="authStore.errors.password" class="flex">
+                  <span class="text-red-400 text-sm m-2 p-2">{{
+                    authStore.errors.password[0]
+                  }}</span>
+                </div>
+        </div>
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Confrime Password</label>
+          </div>
+          <div class="mt-2">
+            <input id="password" name="password" type="password" v-model="form.password_confirmation" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
+        </div>
+      </form>
+
+      <p class="mt-10 text-center text-sm text-gray-500">
+        {{ ' ' }}
+        <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign In </a>
+      </p>
+    </div>
+  </div>
+  </section>
+</template>
