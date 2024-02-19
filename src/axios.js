2173a0aa9b8ed3e110bb240ada4ca814d0c87f13
mohammadhayaty <103389537+mohammadhayaty@users.noreply.github.com>
2024-02-19 23:50:44 +0330

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://laravel-production-3d7b.up.railway.app/";
